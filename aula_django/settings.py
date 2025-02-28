198bb0058a0246f18c7696e2f0fe20aecbaaea9a
thiago_martins <thiago86.tech@gmail.com>
2025-02-27 22:00:17 -0300

    Aula_ Django

--- aula_django/settings.py
+++ aula_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%krixdrd1hzq1fe-tl3crxme!*^jw+@dq+zf+slnx0mi)p@yq4"
+SECRET_KEY = "django-insecure-bvgle(q-w7-9ihp1d2z^qny!tp_(3u8+g)-5emi*ba!-=gcaj1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
